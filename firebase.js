aeb6f76dbac08aab027635bb3f305151214fe3c2
Rchmain <153584856+Rchmain@users.noreply.github.com>
2025-08-09 11:44:42 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnEByvbzaChFPVPULMSNCiIFchgpJghJk",
+  authDomain: "athena-ffca8.firebaseapp.com",
+  projectId: "athena-ffca8",
+  storageBucket: "athena-ffca8.firebasestorage.app",
+  messagingSenderId: "581549682523",
+  appId: "1:581549682523:web:06dfc8048f8a641b29f47e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
